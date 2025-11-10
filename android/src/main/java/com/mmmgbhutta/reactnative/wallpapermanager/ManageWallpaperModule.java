ebe409a13d0a10addd9aee993edcf6b141e553e7
Trias <triasbrata@gmail.com>
2025-11-10 17:34:59 +0700

    bump

--- android/src/main/java/com/mmmgbhutta/reactnative/wallpapermanager/ManageWallpaperModule.java
+++ android/src/main/java/com/mmmgbhutta/reactnative/wallpapermanager/ManageWallpaperModule.java
@@ -1,55 +1,28 @@
 package com.mmmgbhutta.reactnative.wallpapermanager;
 
-import android.app.Activity;
 import android.app.WallpaperManager;
 import android.net.Uri;
 import android.os.Build;
-import android.util.Log;
-import android.util.Base64;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
 
-import androidx.annotation.Nullable;
-
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.DecodeFormat;
-import com.bumptech.glide.load.model.GlideUrl;
-import com.bumptech.glide.load.model.LazyHeaders;
-import com.bumptech.glide.request.RequestOptions;
-import com.bumptech.glide.request.target.CustomTarget;
-import com.bumptech.glide.request.transition.Transition;
+import java.io.InputStream;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
-
 import com.facebook.react.bridge.ReadableMap;
-import com.facebook.react.bridge.ReadableMapKeySetIterator;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.bridge.Arguments;
-import com.facebook.react.views.imagehelper.ResourceDrawableIdHelper;
-import com.facebook.react.views.imagehelper.ImageSource;
 
 public class ManageWallpaperModule extends ReactContextBaseJavaModule {
 
     private final ReactApplicationContext reactContext;
     private WallpaperManager wallpaperManager;
-    private Callback rctCallback = null;
-    private ReadableMap rctParams;
-    private ResourceDrawableIdHelper mResourceDrawableIdHelper;
-    private Uri mUri;
-    private ReactApplicationContext mApplicationContext;
-    private Activity mCurrentActivity;
 
     public ManageWallpaperModule(ReactApplicationContext reactContext) {
         super(reactContext);
         this.reactContext = reactContext;
-        mApplicationContext = getReactApplicationContext();
-
-        wallpaperManager = WallpaperManager.getInstance(mApplicationContext);
-        mResourceDrawableIdHelper = ResourceDrawableIdHelper.getInstance();
+        wallpaperManager = WallpaperManager.getInstance(reactContext);
     }
 
     @Override
@@ -57,25 +30,13 @@ public class ManageWallpaperModule extends ReactContextBaseJavaModule {
         return "ManageWallpaper";
     }
 
-    public void sendMessage(String status, String msg, String url) {
-        if (rctCallback != null) {
-            WritableMap map = Arguments.createMap();
-            map.putString("status", status);
-            map.putString("msg", msg);
-            map.putString("url", url);
-            rctCallback.invoke(map);
-            rctCallback = null;
-        }
-    }
-
     @ReactMethod
     public void setWallpaper(final ReadableMap params, final String type, Callback callback) {
-
         final String source = params.hasKey("uri") ? params.getString("uri") : null;
-        ReadableMap headers = params.hasKey("headers") ? params.getMap("headers") : null;
+
+        WritableMap map = Arguments.createMap();
 
         if (source == null) {
-            WritableMap map = Arguments.createMap();
             map.putString("status", "error");
             map.putString("msg", "Source URI is required");
             map.putString("url", "");
@@ -83,198 +44,69 @@ public class ManageWallpaperModule extends ReactContextBaseJavaModule {
             return;
         }
 
-        if (rctCallback != null) {
-            WritableMap map = Arguments.createMap();
+        // Parse URI for local storage file
+        Uri uri;
+        try {
+            uri = Uri.parse(source);
+            if (uri.getScheme() == null) {
+                map.putString("status", "error");
+                map.putString("msg", "Invalid URI: missing scheme");
+                map.putString("url", source);
+                callback.invoke(map);
+                return;
+            }
 
+            // Only accept file:// and content:// URIs
+            if (!uri.getScheme().equals("file") && !uri.getScheme().equals("content")) {
+                map.putString("status", "error");
+                map.putString("msg", "Only local file URIs are supported (file:// or content://)");
+                map.putString("url", source);
+                callback.invoke(map);
+                return;
+            }
+        } catch (Exception e) {
             map.putString("status", "error");
-            map.putString("msg", "busy");
+            map.putString("msg", "Invalid URI: " + e.getMessage());
             map.putString("url", source);
             callback.invoke(map);
             return;
         }
 
-        rctCallback = callback;
-        rctParams = params;
-
-        final CustomTarget<Bitmap> customTarget = this.getCustomTarget(source, type);
-        mCurrentActivity = getCurrentActivity();
-
-        if (mCurrentActivity == null) {
-            sendMessage("error", "CurrentActivity is null", source);
-            return;
-        }
-
-        //handle base64
-        if (source != null && source.startsWith("data:image/")) {
-            mCurrentActivity.runOnUiThread(new Runnable() {
-                public void run() {
-                    Util.assertMainThread();
-                    try {
-                        Glide
-                            .with(mApplicationContext)
-                            .asBitmap()
-                            .apply(requestOptions())
-                            .load(Base64.decode(source.replaceAll("data:image\\/.*;base64,", ""), Base64.DEFAULT))
-                            .into(customTarget);
-                    } catch (Exception e) {
-                        sendMessage("error", "Exception in Glide：" + e.getMessage(), source);
-                    }
-                }
-            });
-
-            return;
-        }
-
-        boolean useStorageFile = false ;
-
-        // handle bundled app resources
+        // Read file and set wallpaper
         try {
-            mUri = Uri.parse(source);
-            // Verify scheme is set, so that relative uri (used by static resources) are not handled.
-            if (mUri.getScheme() == null) {
-                mUri = null;
-            } else if(
-                !mUri.getScheme().equals("http") &&
-                !mUri.getScheme().equals("https")
-            ) {
-                useStorageFile = true;
-            }
-        } catch (Exception e) {
-            // ignore malformed uri, then attempt to extract resource ID.
-        }
-
-        if (mUri == null) {
-            ImageSource is = new ImageSource(this.getReactApplicationContext(), source);
-            if (is.isResource()) {
-                int resId = mResourceDrawableIdHelper.getResourceDrawableId(this.getReactApplicationContext(), source);
-                Bitmap mBitmap = BitmapFactory.decodeResource(this.getReactApplicationContext().getResources(), resId);
-
-                try {
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                        if (type.equals("home")) {
-                            wallpaperManager.setBitmap(mBitmap, null, true, WallpaperManager.FLAG_SYSTEM);
-                        } else if (type.equals("lock")) {
-                            wallpaperManager.setBitmap(mBitmap, null, true, WallpaperManager.FLAG_LOCK);
-                        } else {
-                            wallpaperManager.setBitmap(mBitmap, null, true, WallpaperManager.FLAG_LOCK | WallpaperManager.FLAG_SYSTEM);   
-                        }
-                    } else {
-                        wallpaperManager.setBitmap(mBitmap);
-                    }
-                    sendMessage("success", "Set Wallpaper Success", source);
-                } catch (Exception e) {
-                    sendMessage("error", "Exception in SimpleTarget：" + e.getMessage(), source);
-                    return;
-                }
+            InputStream stream = reactContext.getContentResolver().openInputStream(uri);
+            if (stream == null) {
+                map.putString("status", "error");
+                map.putString("msg", "Failed to open file stream");
+                map.putString("url", source);
+                callback.invoke(map);
                 return;
             }
 
-            mUri = is.getUri();
-            mCurrentActivity.runOnUiThread(new Runnable() {
-                public void run() {
-                    Util.assertMainThread();
-                    try{
-                        Glide
-                            .with(mApplicationContext)
-                            .asBitmap()
-                            .apply(requestOptions())
-                            .load(mUri)
-                            .into(customTarget);
-                    } catch (Exception e) {
-                        sendMessage("error", "Exception in Glide：" + e.getMessage(), source);
-                    }
+            // Set wallpaper with version check for separate home/lock support
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                if (type.equals("home")) {
+                    wallpaperManager.setStream(stream, null, false, WallpaperManager.FLAG_SYSTEM);
+                } else if (type.equals("lock")) {
+                    wallpaperManager.setStream(stream, null, false, WallpaperManager.FLAG_LOCK);
+                } else {
+                    wallpaperManager.setStream(stream, null, false, WallpaperManager.FLAG_LOCK | WallpaperManager.FLAG_SYSTEM);
                 }
-            });
-        } else if (useStorageFile) {
-            mCurrentActivity.runOnUiThread(new Runnable() {
-                public void run() {
-                    Util.assertMainThread();
-                    try {
-                        Glide
-                            .with(mApplicationContext)
-                            .asBitmap()
-                            .apply(requestOptions())
-                            .load(mUri)
-                            .into(customTarget);
-                    } catch (Exception e) {
-                        sendMessage("error", "Exception in Glide：" + e.getMessage(), source);
-                    }
-                }
-            });
-        } else {
-            // Handle an http / https address
-            final LazyHeaders.Builder lazyHeaders = new LazyHeaders.Builder();
-
-            if (headers != null) {
-                ReadableMapKeySetIterator it = headers.keySetIterator();
-                Log.d("next headers", String.valueOf(it.hasNextKey()));
-                while (it.hasNextKey()) {
-                    String Key = it.nextKey();
-                    lazyHeaders.addHeader(Key, headers.getString(Key));
-                }
+            } else {
+                wallpaperManager.setStream(stream);
             }
 
-            mCurrentActivity.runOnUiThread(new Runnable() {
-                public void run() {
-                    Util.assertMainThread();
-                    try {
-                        Glide
-                            .with(mApplicationContext)
-                            .asBitmap()
-                            .apply(requestOptions())
-                            .load(new GlideUrl(mUri.toString(), lazyHeaders.build()))
-                            .into(customTarget);
-                    } catch (Exception e) {
-                        sendMessage("error", "Exception in Glide：" + e.getMessage(), source);
-                    }
-                }
-            });
+            stream.close();
+
+            map.putString("status", "success");
+            map.putString("msg", "Set Wallpaper Success");
+            map.putString("url", source);
+            callback.invoke(map);
+        } catch (Exception e) {
+            map.putString("status", "error");
+            map.putString("msg", "Exception: " + e.getMessage());
+            map.putString("url", source);
+            callback.invoke(map);
         }
-    }
-
-    private RequestOptions requestOptions() {
-        return new RequestOptions()
-                .format(DecodeFormat.PREFER_ARGB_8888)
-                .centerCrop();
-    }
-
-    private CustomTarget<Bitmap> getCustomTarget(final String source, final String type) {
-        return new CustomTarget<Bitmap>(1080, 1920) {
-            @Override
-            public void onResourceReady(Bitmap bitmap, Transition<? super Bitmap> transition) {
-                try {
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                        if (type.equals("home")) {
-                            wallpaperManager.setBitmap(bitmap, null, true, WallpaperManager.FLAG_SYSTEM);
-                        } else if (type.equals("lock")) {
-                            wallpaperManager.setBitmap(bitmap, null, true, WallpaperManager.FLAG_LOCK);
-                        } else {
-                            wallpaperManager.setBitmap(bitmap, null, true, WallpaperManager.FLAG_LOCK | WallpaperManager.FLAG_SYSTEM);   
-                        }
-                    } else {
-                        wallpaperManager.setBitmap(bitmap);
-                    }
-                    sendMessage("success", "Set Wallpaper Success", source);
-                } catch (Exception e) {
-                    sendMessage("error", "Exception in SimpleTarget：" + e.getMessage(), source);
-                    return;
-                }
-            }
-
-            @Override
-            public void onLoadCleared(@Nullable Drawable placeholder) {
-
-            }
-
-            @Override
-            public void onLoadFailed(Drawable errorDrawable) {
-                // Do nothing.
-                String errorMessage = "Set Wallpaper Failed：Image not loaded";
-                if(errorDrawable != null) {
-                    errorMessage = "Set Wallpaper Failed：" + errorDrawable.toString();
-                }
-                sendMessage("error", errorMessage, source);
-            }
-        };
     }
 }
